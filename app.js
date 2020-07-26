476e0f375eb41ad4552065acd525b1697e6e0f65
Aydn <aydnhooper1997@gmail.com>
2020-07-26 14:52:40 +1200

    one more commit
    one last one

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ var budgetController = (function () {
     this.id = id;
     this.description = description;
     this.value = value;
+    this.percentage = -1;
+  };
+  Expense.prototype.calcPercentage = function (totalIncome) {
+    if (totalIncome > 0) {
+      this.percentage = Math.round(this.value / totalIncome) * 100;
+    } else {
+      this.percentage = -1;
+    }
+    Expense.prototype.getPercentage = function () {
+      return this.percentage;
+    };
   };
   var calculateTotal = function (type) {
     var sum = 0;
